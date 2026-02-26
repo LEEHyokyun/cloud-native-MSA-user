fa9fca0fe39f7343a9b43d5ad239a57eca5b286b
leehyokyun <gyrbs22@gmail.com>
2026-02-26 12:07:44 +0900

    basic server init

---
+++ src/main/java/com/msa/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.msa.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/")
+    public String user(){
+        return "user";
+    }
+}
